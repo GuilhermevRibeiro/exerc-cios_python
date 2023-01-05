821137c611ff3095e4bedc8a41a0914e4357486c
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-05 12:11:42 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,6 @@
+#Escreva um programa que leia o valor em metros e exiba convertido em centímetros e milímetros
+
+n = float(input('Digite a quantidade de metros: '))
+
+print(f'{n} metros em centímetros é igual a {n*100}')
+print(f'{n} metros em milímetros é igual a {n*1000}')
