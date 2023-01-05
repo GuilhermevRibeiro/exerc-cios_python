dc1dc73879920a92d71b9f368c92b0de70c932f4
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-05 14:51:56 -0300

    Add files via upload

---
+++ ex013.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+salario = float(input('Qual o salário atual: '))
+
+print(f'O salário atual é de: {salario} reais, com o aumento, passa a ser: {salario*1.15 :.2f}')
