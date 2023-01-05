58d31fcad105cf90bbfff0b948c4e3f34bdf798f
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-05 12:30:46 -0300

    Add files via upload

---
+++ ex010.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia quanto dinheiro tem na carteira e mostra quantos dólares ela pode comprar.
+
+n = float(input('Digite quanto você tem na carteira: '))
+
+print(f'Com a quantidade de {n} reais, você consegue comprar {n/5.39 :.2f} dólares')
