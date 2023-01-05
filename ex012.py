92b9a9994b4e273908bfa8ff8222bdbb4df587ac
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-05 14:48:28 -0300

    Add files via upload

---
+++ ex012.py
@@ -0,0 +1,5 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, Com 5% de desconto.
+
+produto = float(input('Digite o preço do produto: '))
+
+print(f'O preço original do produto é: {produto} \n aplicando o desconto de 5%, fica: {produto*0.95 :.2f}')
