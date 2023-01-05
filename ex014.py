652f0ce2007242c1440cd8137a0156f8cc606d42
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-05 14:56:40 -0300

    Add files via upload

---
+++ ex014.py
@@ -0,0 +1,5 @@
+#Escreva um programa que converta uma tempretatura digitada em ºC e converta para ºF
+
+temperatura = float(input('Digite a temperatura em ºC: '))
+
+print(f'A temperatura {temperatura}ºC para fahrenheit é igual a: {(temperatura * 9/5) + 32}ºF')
