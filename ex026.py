13a8b4d45331f967819e3c9c9ada73b5f42785fd
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-17 18:30:51 -0300

    Add files via upload

---
+++ ex026.py
@@ -0,0 +1,10 @@
+""" Faça um programa que leia uma frase pelo teclado e mostre:
+-Quantas vezes aparece a letra "A"
+-Em que posição ela aparece pela primeira vez
+-Em que posição ela aparece pela última vez """
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+
+print(f'A letra A aparece {frase.count("A") } vezes')
+print(f'A letra A aparece pela primeira vez na posição {frase.find("A")+1}')
+print(f'A letra A aparece pela última vez na posição {frase.rfind("A")+1}') #procura a partir do lado direito
