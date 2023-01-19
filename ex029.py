ab07e31525f609835f5acb7c48ba5b8be57c9bf0
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-19 11:43:45 -0300

    Add files via upload

---
+++ ex029.py
@@ -0,0 +1,15 @@
+"""Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo
+que ele foi multado. A multa vai custar 7 reais por cada Km acima do limite"""
+from random import randint
+
+velocidade = randint(40, 120)
+
+
+print(f"A velocidade que o carro passou foi {velocidade}km/h")
+
+if velocidade <= 80:
+    print('Você passou na velocidade adequeada e não será multado')
+else:
+    print("Você será multado")
+    print(f'O valor da multa é: {(velocidade-80)*7} reais.')
+
