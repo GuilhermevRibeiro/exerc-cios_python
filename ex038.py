b10165c3c8de76d4c9ae56e763e58d9ebdf984d0
Guilherme Vitor Ribeiro <85648684+GuilhermevRibeiro@users.noreply.github.com>
2023-01-24 11:38:05 -0300

    Add files via upload

---
+++ ex038.py
@@ -0,0 +1,14 @@
+"""Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela uma mensagem:
+-O primeiro valor é maior
+-O segundo valor é maior
+-Não existe valor maior, os dois são iguais"""
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 > num2:
+    print(f'O primeiro número é {num1} e o segundo {num2}. Logo, o primeiro número é maior')
+elif num2 > num1:
+    print(f'O primeiro número é {num1} e o segundo {num2}. Logo, o segundo número é maior')
+else:
+    print('Os números são iguais.')
